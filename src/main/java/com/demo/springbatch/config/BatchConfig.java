3341607ed8cb95a1d65b9e91bc23833b27cce599
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-05-30 03:40:04 +0300

    Update batch processing configuration and enhance CSV data import

--- src/main/java/com/demo/springbatch/config/BatchConfig.java
+++ src/main/java/com/demo/springbatch/config/BatchConfig.java
@@ -2,6 +2,11 @@ package com.demo.springbatch.config;
 
 import com.demo.springbatch.entity.Student;
 import com.demo.springbatch.repository.StudentRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
@@ -11,21 +16,30 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class BatchConfig {
 
     private final StudentRepository studentRepository;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
 
-    public BatchConfig(StudentRepository studentRepository) {
+    public BatchConfig(StudentRepository studentRepository,
+                       JobRepository jobRepository,
+                       PlatformTransactionManager platformTransactionManager) {
         this.studentRepository = studentRepository;
+        this.jobRepository = jobRepository;
+        this.platformTransactionManager = platformTransactionManager;
     }
 
     @Bean
     public FlatFileItemReader<Student> itemReader() {
         FlatFileItemReader<Student> itemReader = new FlatFileItemReader<>();
         itemReader.setResource(new FileSystemResource("src/main/resources/students.csv"));
-        itemReader.setName("CSV-Reader");
+        itemReader.setName("csvReader");
         itemReader.setLinesToSkip(1);
         itemReader.setLineMapper(linemapper());
         return itemReader;
@@ -44,6 +58,31 @@ public class BatchConfig {
         return itemWriter;
     }
 
+    @Bean
+    public Step importStep() {
+        return new StepBuilder("csv-import", jobRepository)
+                .<Student, Student>chunk(10, platformTransactionManager)
+                .reader(itemReader())
+                .processor(studentProcessor())
+                .writer(itemWriter(studentRepository))
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+
+    @Bean
+    public Job runJob() {
+        return new JobBuilder("import-students", jobRepository)
+                .start(importStep())
+                .build();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
+        asyncTaskExecutor.setConcurrencyLimit(10);
+        return asyncTaskExecutor;
+    }
+
     private LineMapper<Student> linemapper() {
         DefaultLineMapper<Student> lineMapper = new DefaultLineMapper<>();
 
@@ -51,7 +90,7 @@ public class BatchConfig {
         DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
         lineTokenizer.setDelimiter(",");
         lineTokenizer.setStrict(false);
-        lineTokenizer.setNames("id", "name", "lastName", "age");
+        lineTokenizer.setNames("name", "lastName", "age");
 
         // Set the tokenizer to the line mapper
         BeanWrapperFieldSetMapper<Student> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
